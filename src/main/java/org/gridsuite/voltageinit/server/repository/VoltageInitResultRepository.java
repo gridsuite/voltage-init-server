45e84bf2f7a23b7eee41fa97571a8e20051df1ce
antoinebhs <antoine.bouhours@rte-france.com>
2023-09-20 14:57:48 +0200

    Use one transaction for inserting status and results.

--- src/main/java/org/gridsuite/voltageinit/server/repository/VoltageInitResultRepository.java
+++ src/main/java/org/gridsuite/voltageinit/server/repository/VoltageInitResultRepository.java
@@ -90,10 +90,11 @@ public class VoltageInitResultRepository {
     }
 
     @Transactional
-    public void insert(UUID resultUuid, OpenReacResult result, UUID modificationsGroupUuid) {
+    public void insert(UUID resultUuid, OpenReacResult result, UUID modificationsGroupUuid, String status) {
         Objects.requireNonNull(resultUuid);
         if (result != null) {
             resultRepository.save(toVoltageInitResultEntity(resultUuid, result, modificationsGroupUuid));
+            globalStatusRepository.save(toStatusEntity(resultUuid, status));
         }
     }
 
