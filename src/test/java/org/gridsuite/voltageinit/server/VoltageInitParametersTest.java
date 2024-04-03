cf2c51f131349b3c9343a55216c091dbfae8e745
Tristan Chuine <tristan.chuine_externe@rte-france.com>
2024-04-03 11:21:00 +0200

    clean

--- src/test/java/org/gridsuite/voltageinit/server/VoltageInitParametersTest.java
+++ src/test/java/org/gridsuite/voltageinit/server/VoltageInitParametersTest.java
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -40,7 +39,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@DirtiesContext
 @AutoConfigureMockMvc
 @Transactional
 public class VoltageInitParametersTest {
