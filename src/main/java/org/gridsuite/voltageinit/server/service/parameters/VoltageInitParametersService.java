8d9917a0c5d13e7c2e179dc7408e6ef24c68f218
Charly B <47325354+EstherDarkish@users.noreply.github.com>
2025-05-16 11:48:34 +0200

    Upgrade to powsybl-dependencies v2025.0.1 (#102)
    Signed-off-by: BOUTIER Charly <charly.boutier@rte-france.com>

--- src/main/java/org/gridsuite/voltageinit/server/service/parameters/VoltageInitParametersService.java
+++ src/main/java/org/gridsuite/voltageinit/server/service/parameters/VoltageInitParametersService.java
@@ -217,7 +217,7 @@ public class VoltageInitParametersService {
     public OpenReacParameters buildOpenReacParameters(VoltageInitRunContext context, Network network) {
         final long startTime = System.nanoTime();
         final ReportNode reportNode = context.getReportNode().newReportNode()
-                                        .withMessageTemplate("VoltageInitParameters", "VoltageInit parameters")
+                                        .withMessageTemplate("voltage.init.server.VoltageInitParameters")
                                         .withTypedValue("parameters_id", Objects.toString(context.getParametersUuid()), "ID")
                                         .add();
         OpenReacParameters parameters = new OpenReacParameters();
@@ -297,7 +297,7 @@ public class VoltageInitParametersService {
     private static void logRestrictedVoltageLevels(final ReportNode reportNode, final Map<String, Double> voltageLevelsIdsRestricted) {
         if (!voltageLevelsIdsRestricted.isEmpty()) {
             reportNode.newReportNode()
-                    .withMessageTemplate("restrictedVoltageLevels", "The modifications to the low limits for certain voltage levels have been restricted to avoid negative voltage limits: ${joinedVoltageLevelsIds}")
+                    .withMessageTemplate("voltage.init.server.restrictedVoltageLevels")
                     .withUntypedValue("joinedVoltageLevelsIds", voltageLevelsIdsRestricted
                             .entrySet()
                             .stream()
@@ -312,12 +312,12 @@ public class VoltageInitParametersService {
                                                              final MutableInt counterMissingVoltageLimits,
                                                              final MutableInt counterVoltageLimitModifications) {
         reportNode.newReportNode()
-                .withMessageTemplate("missingVoltageLimits", "Missing voltage limits of ${nbMissingVoltageLimits} voltage levels have been replaced with user-defined default values.")
+                .withMessageTemplate("voltage.init.server.missingVoltageLimits")
                 .withUntypedValue("nbMissingVoltageLimits", counterMissingVoltageLimits.longValue())
                 .withSeverity(TypedValue.INFO_SEVERITY)
                 .add();
         reportNode.newReportNode()
-                .withMessageTemplate("voltageLimitModifications", "Voltage limits of ${nbVoltageLimitModifications} voltage levels have been modified according to user input.")
+                .withMessageTemplate("voltage.init.server.voltageLimitModifications")
                 .withUntypedValue("nbVoltageLimitModifications", counterVoltageLimitModifications.longValue())
                 .withSeverity(TypedValue.INFO_SEVERITY)
                 .add();
@@ -342,7 +342,7 @@ public class VoltageInitParametersService {
                 final double initialLowVoltageLimit = voltageLevel.getLowVoltageLimit();
                 final double initialHighVoltage = voltageLevel.getHighVoltageLimit();
                 reporter.newReportNode()
-                        .withMessageTemplate("voltageLimitModified", "Voltage limits of ${voltageLevelId} modified: low voltage limit = ${lowVoltageLimit}, high voltage limit = ${highVoltageLimit}")
+                        .withMessageTemplate("voltage.init.server.voltageLimitModified")
                         .withTypedValue("voltageLevelId", voltageLevel.getId(), TypedValue.VOLTAGE_LEVEL)
                         .withTypedValue("lowVoltageLimit", computeRelativeVoltageLevel(initialLowVoltageLimit, voltageLimits.get(VoltageLimitType.LOW_VOLTAGE_LIMIT)), TypedValue.VOLTAGE)
                         .withTypedValue("highVoltageLimit", computeRelativeVoltageLevel(initialHighVoltage, voltageLimits.get(VoltageLimitType.HIGH_VOLTAGE_LIMIT)), TypedValue.VOLTAGE)
