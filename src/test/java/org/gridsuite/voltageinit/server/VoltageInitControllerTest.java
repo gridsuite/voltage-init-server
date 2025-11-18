83a85a7ed797838ec48e87734cb5c860316d44f8
Franck LECUYER <franck.lecuyer@rte-france.com>
2025-11-18 11:00:12 +0100

    Fix compilation pb
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/test/java/org/gridsuite/voltageinit/server/VoltageInitControllerTest.java
+++ src/test/java/org/gridsuite/voltageinit/server/VoltageInitControllerTest.java
@@ -253,7 +253,12 @@ class VoltageInitControllerTest {
             Map<String, List<String>> substationProperty,
             List<Country> countryCodes,
             List<UUID> genericFiltersUuid) throws JsonProcessingException {
-        GlobalFilter globalFilter = new GlobalFilter(nominalVs, countryCodes, genericFiltersUuid, null, substationProperty);
+        GlobalFilter globalFilter = new GlobalFilter();
+        globalFilter.setNominalV(nominalVs);
+        globalFilter.setCountryCode(countryCodes);
+        globalFilter.setGenericFilter(genericFiltersUuid);
+        globalFilter.setSubstationProperty(substationProperty);
+
         return new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL).writeValueAsString(globalFilter);
     }
 
